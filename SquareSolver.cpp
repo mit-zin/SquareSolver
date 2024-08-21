8c6617cf1ca72d09c80cae2f5a9832ead199e1e7
Dmitriy Zinovyev <mit2005zin@yandex.ru>
2024-08-21 14:51:52 +0300

    Добавил тестирование

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -8,12 +8,17 @@ int NotSquareSolver(double b, double c, double* x);
 void entering(double* a, double* b, double* c);
 void ans(int rootsn, double x1, double x2);
 double MinusNullCheck(double num);
+int tester(void);
+int nTester(int nTest, double a, double b, double c, double x1Right, double x2Right, int rootsn);
 
 const int SS_ENF = -1;
 const double EPSILON = 0.000001;
 
 int main(void)
 {
+    int nErrors = tester();
+    printf("%d\n", nErrors);
+
     double a = NAN, b = NAN, c = NAN;
 
     entering(&a, &b, &c);
@@ -59,8 +64,8 @@ int SquareSolver(double a, double b, double c, double* x1, double* x2)  //случай
 
     if (d > 0)
     {
-        *x1 = MinusNullCheck((-b - sqrt(d)) / 2 / a);
-        *x2 = MinusNullCheck((-b + sqrt(d)) / 2 / a);
+        *x1 = min(MinusNullCheck((-b - sqrt(d)) / 2 / a), MinusNullCheck((-b + sqrt(d)) / 2 / a));
+        *x2 = max(MinusNullCheck((-b - sqrt(d)) / 2 / a), MinusNullCheck((-b + sqrt(d)) / 2 / a));
         return 2;
     }
     else if (fabs(d) < EPSILON)
@@ -124,5 +129,43 @@ double MinusNullCheck(double num)   //меняет -0 на 0
     else
     {
         return num;
+    }
+}
+
+int tester(void)    //тестирует solver()
+{
+    int nErrors = 0;
+
+    nErrors += nTester(1, 0.0, 0.0, 0.0, NAN, NAN, SS_ENF);
+    nErrors += nTester(2, 0.0, 0.0, 1.0, NAN, NAN, 0);
+    nErrors += nTester(3, 0.0, 2.0, 0.0, 0.0, NAN, 1);
+    nErrors += nTester(4, 0.0, 2.0, 3.0, -1.5, NAN, 1);
+    nErrors += nTester(5, 0.0, 1.0, -5.0, 5.0, NAN, 1);
+    nErrors += nTester(6, 0.0, 1.5, 2.25, -1.5, NAN, 1);
+    nErrors += nTester(7, 2.0, 0.0, 0.0, 0.0, NAN, 1);
+    nErrors += nTester(8, -3.0, 0.0, 27.0, -3.0, 3.0, 2);
+    nErrors += nTester(9, 1.0, 0.0, 4.0, NAN, NAN, 0);
+    nErrors += nTester(10, 0.7, 0.525, -1.75, -2.0, 1.25, 2);
+    nErrors += nTester(11, 3.0, -6.0, 3.0, 1.0, NAN, 1);
+
+    return nErrors;
+}
+
+int nTester(int nTest, double a, double b, double c, double x1Right, double x2Right, int rootsnRight)   //n-ый тест
+{
+    double x1 = NAN, x2 = NAN;
+    int rootsn = solver(a, b, c, &x1, &x2);
+
+    if (rootsn != rootsnRight || fabs(x1 - x1Right) > EPSILON || fabs(x2 - x2Right) > EPSILON)
+    {
+        printf("Error Test %d: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, rootsn = %d\n"
+               "Expected: x1 = %lg, x2 = %lg, rootsn = %d\n",
+               nTest, a, b, c, x1, x2, rootsn, x1Right, x2Right, rootsnRight);
+
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
