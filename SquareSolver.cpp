9119f53c61d64b4fde8d3601b48afab087c58be2
Dmitriy Zinovyev <mit2005zin@yandex.ru>
2024-08-22 07:22:59 +0300

    Немного исправлений, добавил ещё функций

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,6 +1,7 @@
 #include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 int solver(double a, double b, double c, double* x1, double* x2);
 int SquareSolver(double a, double b, double c, double* x1, double* x2);
@@ -10,9 +11,20 @@ void ans(int rootsn, double x1, double x2);
 double MinusNullCheck(double num);
 int tester(void);
 int nTester(int nTest, double a, double b, double c, double x1Right, double x2Right, int rootsn);
+bool DoubleZeroCheck(double num);
+void SortRoots(double* x1, double* x2);
+void bufferClear(void);
 
-const int SS_ENF = -1;
+//Читать прату, указатели массивы строки и структуры
+
 const double EPSILON = 0.000001;
+const int SUCCESS_END = 0;
+const int NO_ROOTS = 0;
+const int ONE_ROOT = 1;
+const int TWO_ROOTS = 2;
+const int INF_ROOTS = -1;
+const int ONE_ERROR = 1;
+const int NO_ERRORS = 0;
 
 int main(void)
 {
@@ -30,15 +42,15 @@ int main(void)
 
     ans(rootsn, x1, x2);
 
-    return 0;
+    return SUCCESS_END;
 }
 
 int solver(double a, double b, double c, double* x1, double* x2)    //решает уравнение
 {
-    assert(x1 != NULL);
-    assert(x2 != NULL);
+    assert(x1);
+    assert(x2);
 
-    if (fabs(a) < EPSILON)
+    if (DoubleZeroCheck(a))
     {
         return LinearSolver(b, c, x1);
     }
@@ -50,24 +62,24 @@ int solver(double a, double b, double c, double* x1, double* x2)    //решает ура
 
 int LinearSolver(double b, double c, double* x)  //случай если коэфицент a равен 0
 {
-    assert(x != NULL);
+    assert(x);
 
-    if (fabs(b) < EPSILON)
+    if (DoubleZeroCheck(b))
     {
-        return (fabs(c) < EPSILON) ? SS_ENF : 0;
+        return (DoubleZeroCheck(c)) ? INF_ROOTS : NO_ROOTS;
     }
     else
     {
         *x = MinusNullCheck(-c / b);
-        return 1;
+        return ONE_ROOT;
     }
 }
 
 int SquareSolver(double a, double b, double c, double* x1, double* x2)  //случай если коэфицент a не равен 0
 {
-    assert(fabs(a) > EPSILON);
-    assert(x1 != NULL);
-    assert(x2 != NULL);
+    assert(!DoubleZeroCheck(a));
+    assert(x1);
+    assert(x2);
 
     double d = b * b - 4 * a * c;
 
@@ -75,25 +87,25 @@ int SquareSolver(double a, double b, double c, double* x1, double* x2)  //случай
     {
         *x1 = min(MinusNullCheck((-b - sqrt(d)) / 2 / a), MinusNullCheck((-b + sqrt(d)) / 2 / a));
         *x2 = max(MinusNullCheck((-b - sqrt(d)) / 2 / a), MinusNullCheck((-b + sqrt(d)) / 2 / a));
-        return 2;
+        return TWO_ROOTS;
     }
-    else if (fabs(d) < EPSILON)
+    else if (DoubleZeroCheck(d))
     {
         *x1 = MinusNullCheck(-b / 2 / a);
 
-        return 1;
+        return ONE_ROOT;
     }
     else
     {
-        return 0;
+        return NO_ROOTS;
     }
 }
 
 void input(double* a, double* b, double* c)  //ввод
 {
-    assert(a != NULL);
-    assert(b != NULL);
-    assert(c != NULL);
+    assert(a);
+    assert(b);
+    assert(c);
 
     printf("Я решаю квадратные уравнения.\n");
     printf("Уравнение имеет вид ax^2 + bx + c = 0\n");
@@ -101,8 +113,8 @@ void input(double* a, double* b, double* c)  //ввод
 
     while (scanf("%lf %lf %lf", a, b, c) != 3)
     {
-        while (getchar() != '\n');
-
+        bufferClear();                         //TODO: функция очистки буффера
+            // EOF
         printf("Введите числа правильно\n");
     }
 }
@@ -111,20 +123,16 @@ void ans(int rootsn, double x1, double x2)    //вывод
 {
     switch (rootsn)
     {
-        case 0 :
+        case NO_ROOTS :
             printf("Уравнение не имеет корней.");
             break;
-        case 1 :
-            if (fabs(x1) < EPSILON)
-            {
-                x1 = 0.0;
-            }
+        case ONE_ROOT :
             printf("Уравнение имеет один корень: %g", x1);
             break;
-        case 2 :
+        case TWO_ROOTS :
             printf("Уравнение имеет два корня: %g и %g", x1, x2);
             break;
-        case SS_ENF :
+        case INF_ROOTS :
             printf("Уравнение имеет бесконечное количество корней");
             break;
         default :
@@ -134,7 +142,7 @@ void ans(int rootsn, double x1, double x2)    //вывод
 
 double MinusNullCheck(double num)   //меняет -0 на 0
 {
-    if (fabs(num) < EPSILON)
+    if (DoubleZeroCheck(num))
     {
         return fabs(num);
     }
@@ -148,17 +156,17 @@ int tester(void)    //тестирует solver()
 {
     int nErrors = 0;
 
-    nErrors += nTester(1, 0.0, 0.0, 0.0, NAN, NAN, SS_ENF);
-    nErrors += nTester(2, 0.0, 0.0, 1.0, NAN, NAN, 0);
-    nErrors += nTester(3, 0.0, 2.0, 0.0, 0.0, NAN, 1);
-    nErrors += nTester(4, 0.0, 2.0, 3.0, -1.5, NAN, 1);
-    nErrors += nTester(5, 0.0, 1.0, -5.0, 5.0, NAN, 1);
-    nErrors += nTester(6, 0.0, 1.5, 2.25, -1.5, NAN, 1);
-    nErrors += nTester(7, 2.0, 0.0, 0.0, 0.0, NAN, 1);
-    nErrors += nTester(8, -3.0, 0.0, 27.0, -3.0, 3.0, 2);
-    nErrors += nTester(9, 1.0, 0.0, 4.0, NAN, NAN, 0);
-    nErrors += nTester(10, 0.7, 0.525, -1.75, -2.0, 1.25, 2);
-    nErrors += nTester(11, 3.0, -6.0, 3.0, 1.0, NAN, 1);
+    nErrors += nTester(1, 0.0, 0.0, 0.0, NAN, NAN, INF_ROOTS);
+    nErrors += nTester(2, 0.0, 0.0, 1.0, NAN, NAN, NO_ROOTS);
+    nErrors += nTester(3, 0.0, 2.0, 0.0, 0.0, NAN, ONE_ROOT);
+    nErrors += nTester(4, 0.0, 2.0, 3.0, -1.5, NAN, ONE_ROOT);
+    nErrors += nTester(5, 0.0, 1.0, -5.0, 5.0, NAN, ONE_ROOT);
+    nErrors += nTester(6, 0.0, 1.5, 2.25, -1.5, NAN, ONE_ROOT);
+    nErrors += nTester(7, 2.0, 0.0, 0.0, 0.0, NAN, ONE_ROOT);
+    nErrors += nTester(8, -3.0, 0.0, 27.0, 3.0, -3.0, TWO_ROOTS);
+    nErrors += nTester(9, 1.0, 0.0, 4.0, NAN, NAN, NO_ROOTS);
+    nErrors += nTester(10, 0.7, 0.525, -1.75, -2.0, 1.25, TWO_ROOTS);
+    nErrors += nTester(11, 3.0, -6.0, 3.0, 1.0, NAN, ONE_ROOT);
 
     return nErrors;
 }
@@ -168,16 +176,38 @@ int nTester(int nTest, double a, double b, double c, double x1Right, double x2Ri
     double x1 = NAN, x2 = NAN;
     int rootsn = solver(a, b, c, &x1, &x2);
 
+    SortRoots(&x1Right, &x2Right);
+
     if (rootsn != rootsnRight || fabs(x1 - x1Right) > EPSILON || fabs(x2 - x2Right) > EPSILON)
     {
         printf("Error Test %d: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, rootsn = %d\n"
                "Expected: x1 = %lg, x2 = %lg, rootsn = %d\n",
                nTest, a, b, c, x1, x2, rootsn, x1Right, x2Right, rootsnRight);
 
-        return 1;
+        return ONE_ERROR;
     }
     else
     {
-        return 0;
+        return NO_ERRORS;
     }
+}
+
+bool DoubleZeroCheck(double num)    //проверяет, что переменная типа double равна нулю
+{
+    return fabs(num) < EPSILON;
+}
+
+void SortRoots(double* x1, double* x2)  //меняет корни местами, если x1 > x2
+{
+    if (*x1 > *x2)
+    {
+        double change = *x1;
+        *x1 = *x2;
+        *x2 = change;
+    }
+}
+
+void bufferClear(void)
+{
+    while (getchar() != '\n');
 }
